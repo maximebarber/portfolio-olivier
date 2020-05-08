d1f3e7ca48c0badd733294b1040b78d6ef57598a
Maxime <maxime@processmaker.fr>
2020-05-08 05:15:49 +0200

    Init

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+// import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: '#e91e63' },
+    // - 8F2D56 - FBB13C - 73D2DE - 119DA4
+  },
+});
+
+export default theme;
